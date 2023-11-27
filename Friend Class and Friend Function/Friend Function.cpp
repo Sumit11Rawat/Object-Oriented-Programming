e3d41916c7cc26ecbb9760ed7323933513bafbf2
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2023-11-27 07:19:42 +0530

    Create Friend Function.cpp

---
+++ Friend Class and Friend Function/Friend Function.cpp
@@ -0,0 +1,52 @@
+//                           FRIEND FUNCTION
+
+// Data hiding is a fundamental concept of object-oriented programming. 
+
+// It restricts the access of private members from outside of the class.
+
+// Similarly, protected members can only be accessed by derived classes and are inaccessible from outside
+
+// However, there is a feature in C++ called friend functions that break 
+//   this rule and allow us to access member functions from outside the class.
+
+                //  FRIEND FUNCTION
+//  friend class can access private and protected members of other classes in which it is declared as a friend.
+//    Syntax        class rectangle{
+//                    friend void area
+//                      }
+//                   void area(){
+//                     statements
+    //                   }
+#include<iostream>
+using namespace std;
+class rectangle{
+    int length;
+    int breadth;
+    public:
+rectangle(int length,int breadth){
+        this->length=length;
+        this->breadth=breadth;
+    }
+    void show(){
+        cout<<"The length of rectangle is "<<length;
+        cout<<endl;
+        cout<<"The breadth of rectangle is "<<breadth;
+    }
+    friend void area(rectangle &);
+};
+void area(rectangle &t){
+cout<<"The area of rectangle is "<<t.length*t.breadth;
+}
+int main(){
+rectangle x(5,6);
+x.show();
+cout<<endl<<endl;
+area(x);
+
+return 0;
+}
+// output of above code is 
+// The length of rectangle is 5
+// The breadth of rectangle is 6
+
+// The area of rectangle is 30
