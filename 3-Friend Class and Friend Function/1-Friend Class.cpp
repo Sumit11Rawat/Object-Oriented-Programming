f56624a2810393c35ec8b8fe5e9418ab43e7a281
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2023-11-27 08:00:19 +0530

    Update and rename Friend class.cpp to 1-Friend Class.cpp

--- 3-Friend Class and Friend Function/1-Friend Class.cpp
+++ 3-Friend Class and Friend Function/1-Friend Class.cpp
@@ -6,7 +6,7 @@
 
 // Similarly, protected members can only be accessed by derived classes and are inaccessible from outside
 
-// However, there is a feature in C++ called friend functions that break 
+// However, there is a feature in C++ called friend class that break 
 //   this rule and allow us to access member functions from outside the class.
 
                 //  FRIEND CLASS
