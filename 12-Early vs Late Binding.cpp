783ff499c9d37dd22e1ba8a8753f4e53d3fc700e
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2023-12-11 09:10:59 +0530

    Create 12-Early vs Late Binding.cpp

---
+++ 12-Early vs Late Binding.cpp
@@ -0,0 +1,8 @@
+                           EARLY vs LATE BINDING
+
+--> Binding refers to the process of converting identifiers (such as variable and performance names) into addresses.
+
+--> Binding is done for each variable and functions. For functions, it means that matching the call with the right function definition by the compiler.
+    It takes place either at compile time or at runtime.
+
+https://media.geeksforgeeks.org/wp-content/cdn-uploads/cpp-binding.png
